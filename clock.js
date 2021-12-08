a7b36b42ce92d11c1165f5043cf28f37cabb8b79
Zlatan Gavranovic <zlatangavranovic1906@gmail.com>
2021-12-08 08:21:49 +0100

    comments added

--- clock.js
+++ clock.js
@@ -4,10 +4,16 @@ let digit1 = 0;
 let digit2 = 0;
 let digit3 = 0;
 
+//  the event listener starts a function by loading the page
 
 window.addEventListener('load', function(){
+    // the setInterval function starts the clockDate function after every 1000 milisoconds
     setInterval(clockDate, 1000);
 
+    /*   
+        The clockDate function uses the dateConstructor in the clock variable and so we have
+        Access to the current date with methods like getMinutes, getSeconds etc ...
+    */
         function clockDate(){
             let clock = new Date();
             let seconds = clock.getSeconds();
@@ -29,6 +35,9 @@ window.addEventListener('load', function(){
             }else{
                 digit3 = hours;
             }
+
+            //  at the end, we just displays the current time how we like to do it
+
         output.innerHTML = '<h1>' + digit3 + ':' + digit2 + ':' + digit1 + '</h1>';
         }
     
